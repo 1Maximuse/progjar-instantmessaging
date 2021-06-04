2a4c155c3c817d39820b7902eeeb7ecfdf56b395
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2021-06-04 22:47:49 +0700

    added sending and receiving file functionality

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import sys
 from threading import Lock, Thread
@@ -13,26 +14,43 @@ def receive_message(sock_client):
         data = sock_client.recv(65535)
         if len(data) == 0:
             break
-        status = data.decode('utf-8').split("|", 1)[0]
+        status, message = data.split(b"|", 1)
+        status = status.decode('utf-8')
+        if status != '_acceptfile':
+            message = message.decode('utf-8')
         
         if status == "_request":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rReceived friend request from {username}, enter _acc {username} to accept.\n----------\n>> ', end='')
+            print(f'\rReceived friend request from {message}, enter _acc {message} to accept.\n----------\n>> ', end='')
         elif status == "_requestcreated":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rFriend request to {username} sent.\n----------\n>> ', end='')
+            print(f'\rFriend request to {message} sent.\n----------\n>> ', end='')
         elif status == "_requestexists":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rYou have already sent {username} a friend request.\n----------\n>> ', end='')
+            print(f'\rYou have already sent {message} a friend request.\n----------\n>> ', end='')
         elif status == "_requestaccepted":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rYour friend request has been accepted by {username}.\n----------\n>> ', end='')
+            print(f'\rYour friend request has been accepted by {message}.\n----------\n>> ', end='')
+        elif status == "_requestsentaccept":
+            print(f'\rYou are now friends with {message}.\n----------\n>> ', end='')
         elif status == "_alreadyfriends":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rYou already have {username} in your friend list.\n----------\n>> ', end='')
+            print(f'\rYou already have {message} in your friend list.\n----------\n>> ', end='')
         elif status == "_notfriends":
-            username = data.decode('utf-8').split("|", 1)[1]
-            print(f'\rYou are not friend with {username} yet.\n----------\n>> ', end='')
+            print(f'\rYou are not friends with {message} yet.\n----------\n>> ', end='')
+        elif status == '_acceptfile':
+            sender, filename, filesize, filedata = message.split(b'|', 3)
+            sender = sender.decode('utf-8')
+            filename = filename.decode('utf-8')
+            filesize = int(filesize.decode('utf-8'))
+            print(f'\rYou received a file from {sender} named {filename} with size of {filesize} bytes.\n----------\n>> ', end='')
+            while len(filedata) < filesize:
+                if filesize - len(filedata) > 65536:
+                    filedata += sock_client.recv(65536)
+                else:
+                    filedata += sock_client.recv(filesize - len(filedata))
+                    break
+            
+            if not os.path.exists('files'):
+                os.mkdir('files')
+            with open(f'files/{filename}', 'wb') as f:
+                f.write(filedata)
+
         else:
             print(data.decode('utf-8'))
 
@@ -49,11 +67,19 @@ def main():
     thread_receive.start()
 
     while True:
-        dest = input('----------\nAvailable commands:\n_bcast <message>\tBroadcast a message\n<recipient> <message>\tSend a message to recipient\n_req <username>\tSend a friend request to username\n_acc <username>\tAccept a friend request from username\n_quit\tExit the app\n----------\n>> ')
+        dest = input('----------\nAvailable commands:\n_bcast <message>\tBroadcast a message\n<recipient> <message>\tSend a message to recipient\n_req <username>\tSend a friend request to username\n_acc <username>\tAccept a friend request from username\n_sendfile <username> <path>\tSend a file at path to username\n_quit\tExit the app\n----------\n>> ')
         command = dest.split(" ", 1)
         if command[0] == '_quit':
             sock_client.close()
             break
+        elif command[0] == '_sendfile':
+            username, path = dest.split(" ", 2)[1:]
+            size = os.path.getsize(path)
+            filedata = f'_sendfile|{username}|{path.split("/")[-1]}|{size}|'.encode('utf-8')
+            with open(path, 'rb') as f:
+                filedata += f.read()
+            
+            sock_client.sendall(filedata)
         else:
             sock_client.send(f'{command[0]}|{command[1]}'.encode('utf-8'))
 
--- server/server.py
+++ server/server.py
@@ -17,6 +17,14 @@ def send_message(friends, sock_sender, sock_destination, username, destination, 
     else:
         sock_sender.send(f'_notfriends|{destination}'.encode('utf-8'))
 
+def send_file(friends, sock_sender, sock_destination, username, destination, name, size, filedata):
+    if (username, destination) in friends and (destination, username) in friends:
+        message = f'_acceptfile|{username}|{name}|{size}|'.encode('utf-8')
+        message += filedata
+        sock_destination.sendall(message)
+    else:
+        sock_sender.send(f'_notfriends|{destination}'.encode('utf-8'))
+
 def add_friend(friends, sock_sender, sock_recipient, sender, recipient):
     if (sender, recipient) in friends and (recipient, sender) in friends:
         sock_sender.send(f'_alreadyfriends|{recipient}'.encode('utf-8'))
@@ -27,9 +35,10 @@ def add_friend(friends, sock_sender, sock_recipient, sender, recipient):
         sock_sender.send(f'_requestcreated|{recipient}'.encode('utf-8'))
         sock_recipient.send(f'_request|{sender}'.encode('utf-8'))
 
-def accept_friend(friends, sock_destination, requester, accepter):
+def accept_friend(friends, sock_sender, sock_destination, requester, accepter):
     friends.add((accepter, requester))
     sock_destination.send(f'_requestaccepted|{accepter}'.encode('utf-8'))
+    sock_sender.send(f'_requestsentaccept|{requester}'.encode('utf-8'))
 
 def serve_client(clients, friends, sock_client, addr_client, username):
     while True:
@@ -37,21 +46,40 @@ def serve_client(clients, friends, sock_client, addr_client, username):
         if len(data) == 0:
             break
 
-        destination, message = data.decode('utf-8').split('|', 1)
-        formatted_message = f'<{username}>: {message}'
-
         message_log = ''
 
+        destination, message = data.split(b'|', 1)
+        destination = destination.decode('utf-8')
+        if destination != '_sendfile':
+            message = message.decode('utf-8')
+
         if destination == '_bcast':
+            formatted_message = f'<{username}>: {message}'
             send_broadcast(clients, friends, formatted_message, username)
             message_log = f'[{username} -> {destination}] {message}'
         elif destination == '_req':
             add_friend(friends, sock_client, clients[message][0], username, message)
             message_log = f'[Friend request: {username} -> {message}]'
         elif destination == '_acc':
-            accept_friend(friends, clients[message][0], message, username)
+            accept_friend(friends, sock_client, clients[message][0], message, username)
             message_log = f'[Friend request accepted: {username} -> {message}]'
+        elif destination == '_sendfile':
+            recipient, name, size, filedata = message.split(b'|', 3)
+            recipient = recipient.decode('utf-8')
+            name = name.decode('utf-8')
+            size = int(size.decode('utf-8'))
+
+            while len(filedata) < size:
+                if size - len(filedata) > 65536:
+                    filedata += sock_client.recv(65536)
+                else:
+                    filedata += sock_client.recv(size - len(filedata))
+                    break
+            
+            send_file(friends, sock_client, clients[recipient][0], username, recipient, name, size, filedata)
+            message_log = f'[{username} -> {recipient}] Sent file {name} of size {size} bytes'
         else:
+            formatted_message = f'<{username}>: {message}'
             send_message(friends, sock_client, clients[destination][0], username, destination, formatted_message)
             message_log = f'[{username} -> {destination}] {message}'
         
@@ -77,4 +105,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
